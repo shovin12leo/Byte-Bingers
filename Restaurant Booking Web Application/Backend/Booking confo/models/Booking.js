23a34b0844921a57d945ae7a46d7a266173b4803
Shovin Michel David <149549372+shovin12leo@users.noreply.github.com>
2025-01-25 21:09:16 +0530

    Add files via upload

--- Restaurant Booking Web Application/Backend/Booking confo/models/Booking.js
+++ Restaurant Booking Web Application/Backend/Booking confo/models/Booking.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const bookingSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  date: { type: Date, required: true },
+  time: { type: String, required: true },
+  guests: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
