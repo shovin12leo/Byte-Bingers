1414bf45d5f17434357f89dd5a30b4a7e34a5b94
Shovin Michel David <149549372+shovin12leo@users.noreply.github.com>
2025-01-26 07:11:45 +0530

    Add files via upload

--- Restaurant Booking Web Application/Backend/src/config/db.js
+++ Restaurant Booking Web Application/Backend/src/config/db.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('MongoDB Connected');
+    console.log("✅ MongoDB Connected!");
   } catch (error) {
-    console.error('MongoDB Connection Failed:', error);
+    console.error("❌ MongoDB Connection Failed:", error);
     process.exit(1);
   }
 };
